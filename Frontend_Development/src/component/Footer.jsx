787f568860a86341d10aae9dbd04a25b16ec6afc
Liannaka Dadi <liannaka.dadi10@gmail.com>
2025-08-04 18:25:54 +0530

    cleaning material linked with cleaning page

--- Frontend_Development/src/component/Footer.jsx
+++ Frontend_Development/src/component/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -14,10 +14,26 @@ const Footer = () => {
           <Col md={3} lg={2} xl={2} className="mb-4">
             <h5 className="text-uppercase fw-bold mb-4">Company</h5>
             <ul className="list-unstyled">
-              <li><a href="#" className="text-white text-decoration-none">About Us</a></li>
-              <li><a href="#" className="text-white text-decoration-none">Our Services</a></li>
-              <li><a href="#" className="text-white text-decoration-none">Privacy Policy</a></li>
-              <li><a href="#" className="text-white text-decoration-none">Affiliate Program</a></li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  Our Services
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  Affiliate Program
+                </a>
+              </li>
               {/* <li>
                 <Link to="/adminlogin" className="admin-button text-white text-decoration-none">Admin</Link>
               </li> */}
@@ -42,8 +58,22 @@ const Footer = () => {
           <Col md={3} lg={2} xl={2} className="mb-4">
             <h5 className="text-uppercase fw-bold mb-4">Online Shop</h5>
             <ul className="list-unstyled">
-              <li><a href="#" className="text-white text-decoration-none">Stationary</a></li>
-              <li><a href="#" className="text-white text-decoration-none">Cleaning Material</a></li>
+              <li>
+                <Link
+                  to="/products/stationary"
+                  className="text-white text-decoration-none"
+                >
+                  Stationary
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/products/cleaning"
+                  className="text-white text-decoration-none"
+                >
+                  Cleaning Material
+                </Link>
+              </li>
             </ul>
           </Col>
 
@@ -51,17 +81,30 @@ const Footer = () => {
           <Col md={4} lg={2} xl={2} className="mb-4">
             <h5 className="text-uppercase fw-bold mb-4">Follow Us</h5>
             <div>
-              <a href="#" className="text-white me-3 fs-5"><i className="fab fa-facebook-f"></i></a>
-              <a href="#" className="text-white me-3 fs-5"><i className="fab fa-twitter"></i></a>
-              <a href="#" className="text-white me-3 fs-5"><i className="fab fa-instagram"></i></a>
-              <a href="#" className="text-white me-3 fs-5"><i className="fab fa-linkedin-in"></i></a>
+              <a href="#" className="text-white me-3 fs-5">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" className="text-white me-3 fs-5">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="text-white me-3 fs-5">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#" className="text-white me-3 fs-5">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
             </div>
           </Col>
 
           {/* Map Section (Side-by-side with border and rounded corners) */}
           <Col md={12} lg={4} xl={4} className="mb-4">
-            <h5 className="text-uppercase fw-bold mb-3 text-center">Office Location</h5>
-            <div className="border rounded overflow-hidden shadow-sm" style={{ height: "200px" }}>
+            <h5 className="text-uppercase fw-bold mb-3 text-center">
+              Office Location
+            </h5>
+            <div
+              className="border rounded overflow-hidden shadow-sm"
+              style={{ height: "200px" }}
+            >
               <iframe
                 title="office-map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d506.57838743243605!2d76.99253118583378!3d28.458552001809977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d17a521c0a303%3A0x8416cd822c3eb5e4!2sShree%20shyam%20enterprises!5e0!3m2!1sen!2sin!4v1754036659735!5m2!1sen!2sin"
